e4fb40a1bb4276e2db0840c2745d2a625608681a
Baylac-jacque Felix <fbaylacj@scinfe132.univ-pau.fr>
2013-12-02 17:07:48 +0100

    Adding interfaces implementation.

---
+++ Layout.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.PauWare.PauWare_view;
+
+/**
+ *
+ * @author fbaylacj
+ */
+public class Layout implements ILayout{
+    @Override
+    public void addPosition(AbstractElement elem, Position pos){
+        
+    }
+    
+    @Override
+    public void addPosition(AbstractElement elem, float x, float y){
+        
+    }
+    
+    @Override
+    public void removePosition(AbstractElement elem){
+        
+    }
+    
+    @Override
+    public Position getPosition(AbstractElement elem){
+        return new Position();
+    }
+    
+}
