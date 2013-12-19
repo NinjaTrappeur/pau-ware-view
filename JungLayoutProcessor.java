59aa62e0e9f1bd6b997670f5c9c5eb8c94b821ca
shinhermit <josuaharon@hotmail.com>
2013-12-19 15:40:02 +0100

    Adding a Layout processor

---
+++ JungLayoutProcessor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.PauWare.PauWare_view;
+
+/**
+ *
+ * @author josuah
+ */
+public class JungLayoutProcessor implements ILayoutProcessor
+{
+    Layout _layout;
+    
+    public void init(IChart chart)
+    {
+        //
+    }
+    
+    public ILayout getLayout()
+    {
+        return _layout;
+    }
+    
+}
