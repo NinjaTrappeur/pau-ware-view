79f3892e0ef0bffe94f43d69e30b7541d0ecbc2a
Baylac-Jacqué Félix <baylac.felix@gmail.com>
2014-01-19 09:21:29 +0100

    Fixing bug of transition layout processor.

--- TransitionLayoutProcessor.java
+++ TransitionLayoutProcessor.java
@@ -17,7 +17,7 @@ public class TransitionLayoutProcessor {
     private IChart _chart;
     
     private HandlePos _getHandle(AbstractElement elem, Transition trans){
-        //Calculing center of the 2 elements of the transition
+        //Calculing center of the 2 elements of the transition + bottom left 
         Position elemPos = _layout.getPosition(elem);
         AbstractElement otherElem;
         if(trans.origin().equals(elem))
@@ -25,41 +25,62 @@ public class TransitionLayoutProcessor {
         else
             otherElem = trans.origin();
         Position otherElemPos = _layout.getPosition(otherElem);
-        Point2D otherElemCenter = new Point2D.Double(otherElemPos.x()+otherElem.width()/2,
+        Point2D otherElemCenter;
+        if(!(otherElem instanceof SuperState))
+            otherElemCenter = new Point2D.Double(otherElemPos.x()+otherElem.width()/2,
                                              otherElemPos.y()+otherElem.length()/2);
+        else // we take bottom handle as center in this case.
+            otherElemCenter = new Point2D.Double(otherElemPos.x()+otherElem.width()/2,
+                                             otherElemPos.y()+otherElem.length());
+        
         Point2D elemCenter = new Point2D.Double(elemPos.x()+elem.width()/2,
                                                 elemPos.y()+ elem.length()/2);
+
+        Point2D bottomLeft = new Point2D.Double(elemPos.x(),elemPos.y()+elem.length());
         
-        //Determinating which handle we should use
-        if(otherElemCenter.getY()+elemCenter.getY() > elemCenter.getX() - otherElemCenter.getX() &&
-                otherElemCenter.getY()+elemCenter.getY() > elemCenter.getX() + otherElemCenter.getX())
+        //Calculating lines position
+        double pos1=(elemCenter.getX()-bottomLeft.getX())*(otherElemCenter.getY()-bottomLeft.getY())-
+                (elemCenter.getY()-bottomLeft.getY())*(otherElemCenter.getX()-bottomLeft.getX());
+        double pos2=(elemCenter.getX()-elemPos.x())*(otherElemCenter.getY()-elemPos.y())-
+                (elemCenter.getY()-elemPos.y())*(otherElemCenter.getX()-elemPos.x());
+        
+        //returning pos
+        if(pos1<0 && pos2<0)
             return HandlePos.TOP;
-        else if(otherElemCenter.getY()+elemCenter.getY() > elemCenter.getX() + otherElemCenter.getX() &&
-                otherElemCenter.getY()+elemCenter.getY() < elemCenter.getX() - otherElemCenter.getX())
+        else if(pos1<0 && pos2>0)
             return HandlePos.LEFT;
-        else if(otherElemCenter.getY()+elemCenter.getY() < elemCenter.getX() + otherElemCenter.getX() &&
-                otherElemCenter.getY()+elemCenter.getY() > elemCenter.getX() - otherElemCenter.getX())
-            return HandlePos.RIGHT;
-        else
+        else if(pos1>0 && pos2>0)
             return HandlePos.BOTTOM;
+        else
+            return HandlePos.RIGHT;
     }
     
-    private Point2D _getHandlePos(AbstractElement elem, HandlePos pos){
+    private Point2D _getHandlePos(AbstractElement elem, HandlePos pos, Transition trans) {
         Point2D handle = new Point2D.Double();
         Position elemPos = _layout.getPosition(elem);
-        switch(pos){
-            case TOP: handle.setLocation(elemPos.x()+elem.width()/2, 
-                                                  elemPos.y());
-                break;
-            case RIGHT: handle.setLocation(elemPos.x()+elem.width(), 
-                                                  elemPos.y()+elem.length()/2);
-                break;
-            case BOTTOM: handle.setLocation(elemPos.x()+elem.width()/2, 
-                                                  elemPos.y()+elem.length());
-                break;
-            case LEFT: handle.setLocation(elemPos.x(), 
-                                                  elemPos.y()+elem.length()/2);
-                break;
+        if ((!(elem instanceof SuperState))) {
+            switch (pos) {
+                case TOP:
+                    handle.setLocation(elemPos.x() + elem.width() / 2,
+                            elemPos.y());
+                    break;
+                case RIGHT:
+                    handle.setLocation(elemPos.x() + elem.width(),
+                            elemPos.y() + elem.length() / 2);
+                    break;
+                case BOTTOM:
+                    handle.setLocation(elemPos.x() + elem.width() / 2,
+                            elemPos.y() + elem.length());
+                    break;
+                case LEFT:
+                    handle.setLocation(elemPos.x(),
+                            elemPos.y() + elem.length() / 2);
+                    break;
+            }
+        } 
+        else {
+            handle.setLocation(elemPos.x() + elem.width() / 2,
+                    elemPos.y() + elem.length());
         }
         return handle;
     }
@@ -83,11 +104,8 @@ public class TransitionLayoutProcessor {
         for (Transition trans : _chart.transitions()) {
             path = new ArrayList();
             if (trans.origin() instanceof State) {
-                if(trans.origin() instanceof SuperState)
-                    handleOrigin = HandlePos.BOTTOM;
-                else
-                    handleOrigin = _getHandle(trans.origin(), trans);
-                posHandleOrigin = _getHandlePos(trans.origin(), handleOrigin);
+                handleOrigin = _getHandle(trans.origin(), trans);
+                posHandleOrigin = _getHandlePos(trans.origin(), handleOrigin, trans);
             } 
             else {
                 posHandleOrigin = _layout.getPosition(trans.origin()).getPoint2D();
@@ -96,11 +114,8 @@ public class TransitionLayoutProcessor {
             }
 
             if (trans.target() instanceof State) {                
-                if(trans.target() instanceof SuperState)
-                    handleTarget = HandlePos.BOTTOM;
-                else
-                    handleTarget = _getHandle(trans.target(), trans);
-                posHandleTarget = _getHandlePos(trans.target(), handleTarget);
+                handleTarget = _getHandle(trans.target(), trans);
+                posHandleTarget = _getHandlePos(trans.target(), handleTarget, trans);
             } 
             else {
                 posHandleTarget = _layout.getPosition(trans.target()).getPoint2D();
